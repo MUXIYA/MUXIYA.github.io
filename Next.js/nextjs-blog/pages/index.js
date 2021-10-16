d6f89420b84b08de4ddd7b7091b4152f728edf6a
罗龙坡 <qq1352390432>
2021-10-16 14:25:46 +0800

    优化

--- Next.js/nextjs-blog/pages/index.js
+++ Next.js/nextjs-blog/pages/index.js
@@ -3,7 +3,13 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 import { getSortedPostsData } from "../lib/posts";
+import { createFromIconfontCN } from "@ant-design/icons";
 import Date from "../components/date";
+
+const MyIcon = createFromIconfontCN({
+  scriptUrl: "//at.alicdn.com/t/font_2871124_r7l2yqxzuje.js", // 在 iconfont.cn 上生成
+});
+
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
@@ -18,6 +24,7 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      {/* 头部 */}
       <div className={utilStyles.poem_wrap}>
         <div
           className={`${utilStyles.poem_border} ${utilStyles.poem_left}`}
@@ -27,30 +34,31 @@ export default function Home({ allPostsData }) {
         ></div>
         <h1>念两句诗</h1>
         <div className={utilStyles.poem_sentence}>
-           山有木兮木有枝。心悦君兮君不知。
+          山有木兮木有枝。心悦君兮君不知。
         </div>
-        <div className={utilStyles.poem_info}>
-          【春秋】《越人歌》
-        </div>
+        <div className={utilStyles.poem_info}>【春秋】《越人歌》</div>
       </div>
-      <section className={utilStyles.headingMd}>
+      {/* 简介 */}
+      {/* <section className={utilStyles.headingMd}>
         <p>这里可以填充个人介绍</p>
         <Link href="/posts/first-post">
-          <a> 第一篇文章</a>
+          <a> 查看更多</a>
         </Link>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>内容</h2>
+      </section> */}
+      {/* 内容 */}
+      <section className={utilStyles.headingMd}>
+        {/* <h2 className={utilStyles.headingLg}>内容</h2> */}
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>
-                《{title}》
-                  {/* {id} */}
-                </a>
-              </Link>
+              <div>
+                <MyIcon className={utilStyles.svg} type="icon-food-strawberry" />
+                <Link href={`/posts/${id}`} className={utilStyles.title_link}>
+                  <a className={utilStyles.title_a}>{title}</a>
+                </Link>
+              </div>
               <br />
+              <div>摘要</div>
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
               </small>
@@ -58,6 +66,7 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+
       <script
         type="text/javascript"
         src="https://cdn.bootcss.com/canvas-nest.js/1.0.1/canvas-nest.min.js"
